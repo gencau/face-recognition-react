a2ff12de2b0cfd2732a5bad7d92f85e14f891d23
Gen <gencau@gmail.com>
2024-01-29 13:07:39 -0500

    Created client and server directories, to integrate server

---
+++ server/controllers/image.js
@@ -0,0 +1,61 @@
+
+
+getRequestOptions = (imageUrl) =>  {
+    const PAT = 'f2116ae04aef4f2cb1c301095c88a9fa'
+    //Specify the correct user_id/app_id pairings
+    //Since you're making inferences outside your app's scope
+    const USER_ID = 'gcaumart'
+    const APP_ID = 'face-recognition'
+    //Change these to whatever model and image URL you want to use
+    const IMAGE_URL = imageUrl;
+
+    const raw = JSON.stringify({
+      "user_app_id": {
+          "user_id": USER_ID,
+          "app_id": APP_ID
+      },
+      "inputs": [
+          {
+              "data": {
+                  "image": {
+                      "url": IMAGE_URL
+                  }
+              }
+          }
+      ]
+    });
+  
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Key ' + PAT
+      },
+      body: raw
+    };
+    return requestOptions;
+}
+
+const handleApiCall = (req, res) => {
+    fetch("https://api.clarifai.com/v2/models/face-detection/outputs", getRequestOptions(req.body.input))
+    .then(response => response.json())
+    .then(data => res.json(data))
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0].entries);
+    })
+    .catch(err => res.status(400).json('unable to get entries'));
+}
+
+module.exports = {
+    handleImage: handleImage,
+    handleApiCall: handleApiCall
+}
